3222d41f98c83c2f4d83d0ad88f50956e0575668
JaRcomGitHub <jarcom@ukr.net>
2023-02-02 16:48:19 +0200

    ws client example

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
+const http = require('http');
+const WebSocket = require('ws');
 
-
-const WebSocketServer = new require('ws');
-
-const wss = new WebSocketServer.Server({ port: 8080 });
-
-let clients = [];
-
-wss.on('connection', ws => {
-  let id = clients.length;
-  clients[id] = ws;
-  console.log(`Hoвoe соединение #${id}`);
-  // отправляем клиенту сообщение
-  clients[id].send(`Привет, вам присвоен номер №${id}`);
-  // отправляем все остальным
-  clients.forEach((item, index) => {
-    if (index !== id) {
-      item.send(`К нам присоединился номер - ${id}`);
-    }
-  });
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works! jarcom!!!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
 });
+server.listen();
+console.log("server start");
+    
+
+const ws = new WebSocket('ws://dev.tirascloud.com:4033');
+ws.onmessage = e => {
+  console.log("ws:",e.data);
+};
+console.log("ws start");
--- temp-file/temp.js
+++ temp-file/temp.js
@@ -38,3 +38,58 @@ app.listen(3000, () => {
 
 
 
+
+
+
+const WebSocketServer = new require('ws');
+
+const wss = new WebSocketServer.Server({ port: 8080 });
+
+let clients = [];
+
+wss.on('connection', ws => {
+  let id = clients.length;
+  clients[id] = ws;
+  console.log(`Hoвoe соединение #${id}`);
+  // отправляем клиенту сообщение
+  clients[id].send(`Привет, вам присвоен номер №${id}`);
+  // отправляем все остальным
+  clients.forEach((item, index) => {
+    if (index !== id) {
+      item.send(`К нам присоединился номер - ${id}`);
+    }
+  });
+});
+
+//html
+const ws = new WebSocket('ws://localhost:8080');
+ws.onmessage = e => {
+  console.log(e.data);
+};
+
+
+
+
+
+
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works! jarcom!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen(3000, () => {
+    console.log('server start');
+  });
+
+
+
+
+
+
+
+
+
