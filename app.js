72cddea5281625fb680f1a3521e46be7d2e3726d
JaRcomGitHub <jarcom@ukr.net>
2023-02-03 15:11:57 +0200

    sync files

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-const http = require("http");
-const WebSocket = require("ws");
+const WebSocket1 = require("ws");
 const path = require("path");
 const fs = require("fs/promises");
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  var message = "It works! jarcom!!!\n",
-    version = "NodeJS " + process.versions.node + "\n",
-    response = [message, version].join("\n");
-  res.end(response);
-});
-server.listen();
-console.log("server start");
-
-const ws = new WebSocket("ws://dev.tirascloud.com:4033");
+const ws = new WebSocket1("ws://dev.tirascloud.com:4033");
 ws.onmessage = (e) => {
   toLogFile(e.data);
-  console.log("ws:", e.data);
+  console.log("ws:", new Date().toISOString());
 };
-console.log("ws start");
+console.log("ws start", new Date().toISOString());
 
 async function toLogFile(content) {
   const filename = getCurrentDate() + ".log";
   const logFile = path.resolve(__dirname, "logs", filename);
-  // console.log(logFile);
   try {
-    // const content = "Some content!";
     await fs.appendFile(logFile, content + "\n");
   } catch (err) {
     console.log(err);
---
+++ wstest.js
@@ -0,0 +1,20 @@
+const WebSocketServer = new require('ws');
+
+const wss = new WebSocketServer.Server({ port: 8080 });
+
+let clients = [];
+
+wss.on('connection', ws => {
+  let id = clients.length;
+  clients[id] = ws;
+  console.log(`Hoвoe соединение #${id}`);
+  // отправляем клиенту сообщение
+  clients[id].send(`Привет, вам присвоен номер №${id}`);
+  // отправляем все остальным
+  clients.forEach((item, index) => {
+    if (index !== id) {
+      item.send(`К нам присоединился номер - ${id}`);
+    }
+  });
+});
+console.log("ws server start", new Date().toISOString());
